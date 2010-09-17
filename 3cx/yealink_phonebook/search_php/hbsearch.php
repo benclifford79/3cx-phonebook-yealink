669e5a1c6b3c2ef93d0637d9bbd76aa1e5dcf52e
Ben Clifford <ben@hotel-broadband.com>
2010-09-17 17:02:43 +0200

    First version for testing

---
+++ 3cx/yealink_phonebook/search_php/hbsearch.php
@@ -0,0 +1,109 @@
+#!/usr/bin/php
+
+ $URL = 'http://192.168.0.231/';
+ $per_page = '10'; 
+
+ $mysql_conn = mysql_connect('localhost', 'root', '');
+ mysql_select_db('test', $mysql_conn );
+    
+$NAME=$_GET["name"];
+$FROM=$_GET["FROM"];
+$TO=$_GET["TO"];
+ if ( ($FROM=='') and ($TO=='') )
+ {
+   //check to see how many 
+   $result= mysql_query("SELECT count(phones.fname) as total 
+                         FROM phones 
+                         WHERE phones.fname LIKE '$NAME%' ", $mysql_conn);
+   $howmany = mysql_fetch_row($result);
+
+   if ($howmany[0] > $per_page)
+   {
+    $start = 0; 
+    $index = 0;
+    $total = $howmany[0];
+    $remain = $per_page;
+    print("\n");
+    print("<YealinkIPPhoneDirectory>\n");  
+   
+    while ($start < ($total + 1))
+    { 
+      $limitstart = 'LIMIT '.$start.','.$per_page;
+      $result = mysql_query("SELECT fname,phone 
+                             FROM phones 
+                             WHERE fname LIKE '$NAME%' ORDER BY fname $limitstart", $mysql_conn); 
+
+      $row = mysql_fetch_row($result); 
+      $from = $row[0];
+      if (($total - $start) < $per_page) { $remain = $total - $start; }
+      for ($i = 1; $i < $remain; ++$i) { $row = mysql_fetch_row($result); }
+      $to = $row[0]; 
+    
+      print("<SoftKeyItem>\n"); 
+      print("\t<Name>"); 
+      print($index);         
+      print("</Name>\n"); 
+      print("\t<URL>"); 
+      print($URL."search.php?FROM=".$from."&TO=".$to); 
+      print("</URL>\n"); 
+      print("</SoftKeyItem>\n"); 
+
+      $start = $start + $per_page;
+      $index = $index+1;
+    } 
+    print("</YealinkIPPhoneDirectory>\n"); 
+
+   } else {
+
+    $result = mysql_query("SELECT fname,lname,phone 
+                           FROM phones 
+                           WHERE phones.fname LIKE '$NAME%' 
+                           ORDER BY fname ", $mysql_conn); 
+
+    print("\n");
+    print("<YealinkIPPhoneDirectory>\n");  
+
+    while($row = mysql_fetch_row($result)) 
+    { 
+      print("<DirectoryEntry>\n"); 
+      print("\t<Name>"); 
+      print($row[0].", ".$row[1] ); 
+      print("</Name>\n"); 
+      print("\t<Telephone>"); 
+      print($row[2]); 
+      print("</Telephone>\n"); 
+      print("</DirectoryEntry>\n"); 
+    } 
+    print("</YealinkIPPhoneDirectory>\n"); 
+   }
+   
+   
+
+ } else {
+
+  $result = mysql_query("SELECT fname,lname,phone 
+                         FROM phones 
+                         WHERE fname>='$FROM' AND fname<='$TO' 
+                         ORDER BY fname", $mysql_conn); 
+
+   print("\n");
+   print("<YealinkIPPhoneDirectory>\n");  
+
+   while($row = mysql_fetch_row($result)) 
+   { 
+     print("<DirectoryEntry>\n"); 
+     print("\t<Name>"); 
+     print($row[0].", ".$row[1] ); 
+     print("</Name>\n"); 
+     print("\t<Telephone>"); 
+     print($row[2]); 
+     print("</Telephone>\n"); 
+     print("</DirectoryEntry>\n"); 
+   } 
+   print("</YealinkIPPhoneDirectory>\n"); 
+ }
+
+
+
+
+
---
+++ 3cx/yealink_phonebook/search_php/yealinkphonebook.php
@@ -0,0 +1,136 @@
+#!/usr/bin/php
+<?
+# PHP 5 Script to load 3CX XML phone book and allow searching from Yealink T2X style phones
+# V 0.0.1 Initial commit
+# Ben Clifford ben@hotel-broadband.com 17/09/2010
+
+
+# Vars
+#-------------------------------------------
+$NAME=$_GET["name"];
+$FROM=$_GET["FROM"];
+
+$xml_pb 	= "pb.xml";
+$search_string 	= "B";
+$search_field 	= "display_name"; # Can be display_name, last_name or first_name
+$xml 		= simplexml_load_file($xml_pb);
+
+# Initial contact list parsed from XML
+$contact_list 	= array();
+# Filtered results array
+$results_list 	= array();
+
+
+# Main
+#-------------------------------------------
+
+parse_xml_phonebook($xml);
+
+if ($name) {
+  filter_results($name, true);
+} else {
+  filter_results("", false);
+};
+
+display_results();
+
+
+
+## Subs
+#-------------------------------------------
+
+# Parse XML file
+function parse_xml_phonebook() {
+  global $xml;
+  # Contacts
+  foreach ($xml->children() as $contact) {
+    # Contact items, e.g. Name, number
+    $record = array();
+    $display_name 	= '';
+    $first_name		= '';
+    $last_name		= '';
+    $number 		= '';
+    foreach ($contact->children() as $contact_item) {
+      global $record;
+      global $display_name;
+      global $first_name;
+      global $last_name;
+      global $number;
+            
+      # Pull out DisplayName
+      if (strcmp($contact_item->getName(),"DisplayName") == 0) {
+        //echo "Display Name: $contact_item\n";
+        $display_name = $contact_item;
+      };
+
+      # Pull out FirstName
+      if (strcmp($contact_item->getName(),"FirstName") == 0) {
+        //echo "First Name: $contact_item\n";
+        $first_name = $contact_item;
+      };
+      
+      # Pull out LastName
+      if (strcmp($contact_item->getName(),"LastName") == 0) {
+        //echo "Last Name: $contact_item\n";
+        $last_name = $contact_item;
+      };
+
+      # Pull out Number
+      if (strcmp($contact_item->getName(),"Number") == 0) {
+        //echo "Number: $contact_item\n";
+        $number = $contact_item;
+      };
+    };
+    $record = array("display_name" => $display_name, "first_name" => $first_name, "last_name", "number" => $number);
+    add_to_list($record);
+  };
+};
+
+# Add parsed XML record to global array
+function add_to_list($r) {
+  global $contact_list;
+  array_push($contact_list, $r);
+};
+
+
+# Filter results based on search parameter
+function filter_results($search, $do_search) {
+ global $contact_list;
+ global $results_list;
+ global $search_field;
+ $str_length = strlen($search);
+ foreach($contact_list as $c) {
+  if ($do_search) {
+    if (preg_match("/^${search}.*/i", $c[$search_field], $matches)) {
+      //print "Search matched $search\n";
+      array_push($results_list, $c);
+    };
+  } else {
+     # No search criteria, push the whole list
+     array_push($results_list, $c);
+  };
+ };
+};
+
+
+# Returns results to caller based on required format
+function display_results() {
+ global $results_list;
+  print("<YealinkIPPhoneDirectory>\n");  
+    foreach($results_list as $c)
+    { 
+      print("<DirectoryEntry>\n"); 
+      print("\t<Name>"); 
+      print($c['display_name']); 
+      print("</Name>\n"); 
+      print("\t<Telephone>"); 
+      print($c['number']); 
+      print("</Telephone>\n"); 
+      print("</DirectoryEntry>\n"); 
+    } 
+    print("</YealinkIPPhoneDirectory>\n"); 
+};
+
+
+
+?>
